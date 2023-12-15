08c6d7531e99a3e3330c0befab709dce16359d93
LeDucHoang2002 <leduchoang.10022002@gmail.com>
2023-12-16 01:04:10 +0700

    update

--- resources/views/admin/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span> &copy; Bản quyền thuộc SEASIDE STORE</span>
-        </div>
-    </div>
-</footer>
